b964fc0c3c448adde0e29f8ea293d448abec0ce6
Nisha Ramesh Gowda <nishagowda191@gmail.com>
2026-07-14 19:41:39 +0530

    Added Day 6 p1.py

--- class/IncrementalProject/models/Class/IncrementalProject/models/base_product.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from models.metaclass import ProductMeta
-
-
-class BaseProduct(ABC, metaclass=ProductMeta):
-
-    @abstractmethod
-    def display_details(self):
-        pass
